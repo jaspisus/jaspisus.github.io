7760148e4dd74feea30d3edc4d0c27245011b42a
jasperus <m.jackowski@outlook.com>
2021-07-04 11:59:54 +0200

    improved calculator

--- calculator/script/main.js
+++ calculator/script/main.js
@@ -1,6 +1,9 @@
 const buttonsBox = document.querySelector('.calc__buttons__input');
 const display = document.querySelector('.calc__display');
 const equalBtn = document.querySelector('#equals-btn');
+const resetBtn = document.querySelector('#reset-btn');
+
+const countingOperators = ['DEL', '+', '-', '*', '/', '.'];
 
 const renderButtons = () => {
 	for (let i = 0; i < 10; i++) {
@@ -10,8 +13,6 @@ const renderButtons = () => {
 		digitButton.innerHTML = i;
 		buttonsBox.append(digitButton);
 	}
-
-	const countingOperators = ['DEL', '+', '-', '*', '/', '.'];
 
 	countingOperators.forEach(operator => {
 		const digitButton = document.createElement('button');
@@ -24,22 +25,66 @@ const renderButtons = () => {
 
 renderButtons();
 
-const userInputs = [];
+const userInputs = [0];
 
-const renderDisplay = value => {
-	userInputs.push(value);
+let freshCalculation = true;
+
+const renderDisplay = (value = false) => {
+	if (userInputs.length > 15) return;
+	if (value) {
+		userInputs.push(value);
+	}
 	display.innerHTML = userInputs.join('');
 };
 
+const resetCalculation = () => {
+	freshCalculation = true;
+	userInputs.length = 0;
+	renderDisplay('0');
+};
+
+const backspace = () => {
+	if (userInputs.length == 1 || userInputs.length == 0) {
+		resetCalculation();
+		userInputs.pop();
+	} else {
+		userInputs.pop();
+		renderDisplay();
+	}
+};
+
 const captureValue = e => {
-	renderDisplay(e.target.dataset.value);
+	if (freshCalculation) {
+		userInputs.pop();
+	}
+
+	const clickedBtn = e.target.dataset.value;
+	let parsedClickedBtn;
+
+	if (clickedBtn == 'DEL') {
+		backspace();
+	} else if (countingOperators.includes(clickedBtn)) {
+		parsedClickedBtn = clickedBtn;
+	} else {
+		parsedClickedBtn = parseInt(clickedBtn);
+	}
+
+	if (parsedClickedBtn) {
+		renderDisplay(parsedClickedBtn);
+	}
+
+	freshCalculation = false;
 };
 
 const showResult = () => {
 	const result = eval(userInputs.join(''));
-	display.innerHTML = result.toFixed(10);
+	display.innerHTML = result.toString().slice(0, 16);
+	userInputs.length = 0;
+	userInputs[0] = 0;
 };
 
 buttonsBox.addEventListener('click', captureValue);
 
 equalBtn.addEventListener('click', showResult);
+
+resetBtn.addEventListener('click', resetCalculation);
